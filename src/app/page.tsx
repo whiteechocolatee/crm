8c237a59a26b1654a87d7e0420e490ea7147c253
whiiteechocolatee <creatuseandr@icloud.com>
2024-10-17 20:50:10 +0300

    building session middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
+import { useCurrent } from '@/features/auth/api/use-current';
+import { useLogout } from '@/features/auth/api/use-logout';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push('/sign-in');
+    }
+  }, [data, isLoading, router]);
+
   return (
     <div className="flex gap-4">
-      <Button disabled size="lg" variant="primary">
-        Button
-      </Button>
-      <Button variant="destructive">Button</Button>
-      <Button size="sm" variant="secondary">
-        Button
-      </Button>
-      <Button variant="ghost">Button</Button>
-      <Button variant="teritary">Button</Button>
-      <Button variant="muted">Button</Button>
+      Only for authenticated users
+      <Button onClick={() => mutate()}>Log out</Button>
     </div>
   );
 }
---
+++ src/lib/session-middleware.ts
@@ -0,0 +1,55 @@
+import 'server-only';
+
+import { getCookie } from 'hono/cookie';
+import { createMiddleware } from 'hono/factory';
+import {
+  Account,
+  Client,
+  Databases,
+  Models,
+  Storage,
+  type Account as AccountType,
+  type Databases as DatabasesType,
+  type Storage as StorageType,
+  type Users as UsersType,
+} from 'node-appwrite';
+import { AUTH_COOKIE } from '@/features/auth/constants';
+
+type sessionMiddlewareContext = {
+  Variables: {
+    account: AccountType;
+    databases: DatabasesType;
+    storage: StorageType;
+    users: UsersType;
+    user: Models.User<Models.Preferences>;
+  };
+};
+
+export const sessionMiddleware = createMiddleware<sessionMiddlewareContext>(
+  async (c, next) => {
+    const client = new Client()
+      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+
+    const session = getCookie(c, AUTH_COOKIE);
+
+    if (!session) {
+      return c.json({ error: 'Unauthorized' }, 401);
+    }
+
+    client.setSession(session);
+
+    const account = new Account(client);
+    const databases = new Databases(client);
+    const storage = new Storage(client);
+
+    const user = await account.get();
+
+    c.set('user', user);
+    c.set('account', account);
+    c.set('storage', storage);
+    c.set('databases', databases);
+
+    await next();
+  },
+);
