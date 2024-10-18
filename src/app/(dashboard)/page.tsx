72803b4fb7ad2b37df515a54ff45066c369e8a6a
whiiteechocolatee <creatuseandr@icloud.com>
2024-10-18 15:29:25 +0300

    display createWorkspaceForm component

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,4 +1,5 @@
 import { getCurrent } from '@/features/auth/actions';
+import CreateWorkspaceForm from '@/features/workspaces/components/create-workspace-form';
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
@@ -8,5 +9,9 @@ export default async function Home() {
     redirect('/sign-in');
   }
 
-  return <div className="flex gap-4">This is a home page</div>;
+  return (
+    <div className="bg-neutral-500 p-4 h-full">
+      <CreateWorkspaceForm />
+    </div>
+  );
 }
