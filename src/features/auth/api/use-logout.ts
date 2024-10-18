968a639189f9dde5e076f64beea5c5cbc04f736c
whiiteechocolatee <creatuseandr@icloud.com>
2024-10-18 15:28:06 +0300

    add toast and handle response errors

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -2,6 +2,7 @@ import { client } from '@/lib/rpc';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { InferResponseType } from 'hono';
 import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 
 type ResponseType = InferResponseType<(typeof client.api.auth.logout)['$post']>;
 
@@ -13,11 +14,19 @@ export const useLogout = () => {
     mutationFn: async () => {
       const response = await client.api.auth.logout.$post();
 
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+
       return await response.json();
     },
     onSuccess: () => {
+      toast.success('Logged out!');
       router.refresh();
       queryClient.invalidateQueries({ queryKey: ['current'] });
+    },
+    onError: () => {
+      toast.error('Failed to logout!');
     },
   });
 
