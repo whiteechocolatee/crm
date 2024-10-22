a71985a5b01be3bfe1ecd23f12b6984444e195cb
whiiteechocolatee <creatuseandr@icloud.com>
2024-10-22 15:04:17 +0300

    code improvements (clientSession lib)

--- src/features/workspaces/actions.ts
+++ src/features/workspaces/actions.ts
@@ -1,26 +1,13 @@
-import { cookies } from 'next/headers';
-import { Account, Client, Databases, Query } from 'node-appwrite';
-import { AUTH_COOKIE } from '../auth/constants';
 import { DATABASE_ID, MEMBERS_ID, WORKSPACE_ID } from '@/config';
-import { getMember } from './utils';
+import { createSessionClient } from '@/lib/appwrite';
+import { Query } from 'node-appwrite';
 import { WorkspaceType } from './types';
+import { getMember } from './utils';
 
 export const getWorkspaces = async () => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+    const { account, databases } = await createSessionClient();
 
-    const session = await cookies().get(AUTH_COOKIE);
-
-    if (!session) {
-      return { documents: [], total: 0 };
-    }
-
-    client.setSession(session.value);
-
-    const databases = new Databases(client);
-    const account = new Account(client);
     const user = await account.get();
 
     const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
@@ -51,24 +38,12 @@ type GetWorkspaceProps = {
 
 export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+    const { account, databases,  } = await createSessionClient();
 
-    const session = await cookies().get(AUTH_COOKIE);
-
-    if (!session) {
-      return null;
-    }
-
-    client.setSession(session.value);
-
-    const databases = new Databases(client);
-    const account = new Account(client);
     const user = await account.get();
 
     const member = await getMember({
-      databases: new Databases(client),
+      databases,
       workspaceId,
       userId: user.$id,
     });
