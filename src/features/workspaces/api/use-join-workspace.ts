edb85e1e3fc7287c148e6470b0c21d00b7997c2c
whiiteechocolatee <creatuseandr@icloud.com>
2024-10-23 19:16:49 +0300

    route / api for join member to workspace

---
+++ src/features/workspaces/api/use-join-workspace.ts
@@ -0,0 +1,45 @@
+import { client } from '@/lib/rpc';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { InferRequestType, InferResponseType } from 'hono';
+import { toast } from 'sonner';
+
+type ResponseType = InferResponseType<
+  (typeof client.api.workspaces)[':workspaceId']['join']['$post'],
+  200
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.workspaces)[':workspaceId']['join']['$post']
+>;
+
+export const useJoinWorkspace = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ param, json }) => {
+      const response = await client.api.workspaces[':workspaceId']['join'][
+        '$post'
+      ]({
+        param,
+        json,
+      });
+
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+
+      return await response.json();
+    },
+    onSuccess: ({ data }) => {
+      toast.success('Joined workspace!');
+      queryClient.invalidateQueries({ queryKey: ['workspaces'] });
+      queryClient.invalidateQueries({
+        queryKey: ['workspaces', data.workspace.$id],
+      });
+    },
+    onError: () => {
+      toast.error('Failed to join workspace!');
+    },
+  });
+
+  return mutation;
+};
