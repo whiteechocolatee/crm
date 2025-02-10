33871233852a7a74f4a4e54a7325e40958bd6b1c
whiiteechocolatee <creatuseandr@icloud.com>
2025-02-10 20:26:29 +0200

    Add Telegram bot webhook integration and route

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,6 +2,7 @@ import auth from '@/features/auth/server/route';
 import tasks from '@/features/tasks/server/route';
 import members from '@/features/members/server/route';
 import projects from '@/features/projects/server/route';
+import telegram from '@/features/telegram/server/route';
 import workspaces from '@/features/workspaces/server/route';
 
 import { Hono } from 'hono';
@@ -14,6 +15,7 @@ const routes = app
   .route('/tasks', tasks)
   .route('/members', members)
   .route('/projects', projects)
+  .route('/telegram', telegram)
   .route('/workspaces', workspaces);
 
 export const GET = handle(app);
---
+++ src/features/telegram/server/route.ts
@@ -0,0 +1,53 @@
+import { DATABASE_ID, MEMBERS_ID, WORKSPACE_ID } from '@/config';
+import { Hono } from 'hono';
+import { Query, Client, Databases } from 'node-appwrite';
+
+const app = new Hono();
+
+const client = new Client()
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+
+app
+  .post('/webhook', async c => {
+    const { message } = await c.req.json();
+    if (!message) return c.json({ success: false });
+
+    const databases = new Databases(client);
+
+    const chatId = message.chat.id;
+    const userId = message.text.trim();
+
+    if (!userId) {
+      return c.json({
+        method: 'sendMessage',
+        chat_id: chatId,
+        text: 'Неверный userId.',
+      });
+    }
+
+    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+      Query.equal('userId', userId),
+    ]);
+
+    if (members.total === 0) {
+      return c.json({ data: { documents: [], total: 0 } });
+    }
+
+    const workspaceIds = members.documents.map(member => member.workspaceId);
+
+    const workspaces = await databases.listDocuments(
+      DATABASE_ID,
+      WORKSPACE_ID,
+      [Query.orderDesc('$createdAt'), Query.contains('$id', workspaceIds)],
+    );
+
+    return c.json({ data: workspaces });
+  })
+  .get('/', async c => {
+    return c.json({
+      success: true,
+    });
+  });
+
+export default app;
